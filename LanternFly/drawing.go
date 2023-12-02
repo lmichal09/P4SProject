3f483b8fe2fbc5eedf741290a38886d19529fd26
lmichal09 <143451646+lmichal09@users.noreply.github.com>
2023-12-01 20:37:33 -0500

    main

--- LanternFly/drawing.go
+++ LanternFly/drawing.go
@@ -25,9 +25,34 @@ func AnimateSystem(timePoints []Country, canvasWidth, drawingFrequency int) []im
 	return images
 }
 
-// DrawToCanvas generates the image corresponding to a canvas after drawing a Universe
-// object's bodies on a square canvas that is canvasWidth pixels x canvasWidth pixels
-func DrawToCanvas(c Country, canvasWidth int) image.Image {
+// GetFlyColor returns the color for a fly based on its stage
+func GetFlyColor(stage int, isAlive bool) canvas.Color {
+	if !isAlive {
+		return canvas.MakeColor(0, 0, 0) // Black for dead flies
+	}
+
+	// Add color based on the fly's stage
+	switch stage {
+	case 0:
+		return canvas.MakeColor(255, 0, 0) // Red for egg
+	case 1:
+		return canvas.MakeColor(255, 165, 0) // Orange for instar1
+	case 2:
+		return canvas.MakeColor(255, 255, 0) // Yellow for instar2
+	case 3:
+		return canvas.MakeColor(0, 255, 0) // Green for instar3
+	case 4:
+		return canvas.MakeColor(0, 0, 255) // Blue for instar4
+	case 5:
+		return canvas.MakeColor(128, 0, 128) // Purple for adult
+	default:
+		return canvas.MakeColor(255, 255, 255) // White for unknown stage
+	}
+}
+
+// DrawToCanvas generates the image corresponding to a canvas after drawing a Quadrant
+// object's flies on a square canvas that is canvasWidth pixels x canvasWidth pixels
+func DrawToCanvas(country Country, canvasWidth int) image.Image {
 	// set a new square canvas
 	c := canvas.CreateNewCanvas(canvasWidth, canvasWidth)
 
@@ -36,16 +61,21 @@ func DrawToCanvas(c Country, canvasWidth int) image.Image {
 	c.ClearRect(0, 0, canvasWidth, canvasWidth)
 	c.Fill()
 
-	// range over all the bodies and draw them.
-	for _, b := range s.Boids {
-		c.SetFillColor(canvas.MakeColor(255, 60, 25))
-		cx := (b.Position.X / s.Width) * float64(canvasWidth)
-		cy := (b.Position.Y / s.Width) * float64(canvasWidth)
+	// range over all the flies and draw them.
+	for _, fly := range country.Fly {
+		// Get the color based on the fly's stage and alive status
+		color := GetFlyColor(fly.Stage, fly.isAlive)
+
+		// Set the fly color
+		c.SetFillColor(color)
+
+		cx := (fly.Position.X / country.Width) * float64(canvasWidth)
+		cy := (fly.Position.Y / country.Width) * float64(canvasWidth)
 		r := 5
 		c.Circle(cx, cy, float64(r))
 		c.Fill()
-
 	}
+
 	// we want to return an image!
 	return c.GetImage()
 }
