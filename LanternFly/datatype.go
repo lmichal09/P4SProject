b6af66061c72e8b81006238517e4264201107708
shihhsuk <143510222+shihhsuk@users.noreply.github.com>
2023-12-03 23:31:42 -0500

    datatype

--- LanternFly/datatype.go
+++ LanternFly/datatype.go
@@ -1,19 +1,18 @@
 package main
 
 type Country struct {
-	width      float64
-	height     float64
-	flies      []Fly
-	population int
+	width  float64
+	height float64
+	flies  []Fly
+	trees  []OrderedPair
 }
 
 type Fly struct {
-	position, velocity, acceleration OrderedPair
-	stage                            int     // 0 = egg, 1 = instar1, 2 = instar2, 3 = instar3, 4 = instar4, 5 = adult, 6= dead
-	energy                           float64 // Degree-days
-	isAlive                          bool
-	locationID                       int
-	color                            Color // color to show on scatter plot (red, orange, yellow, green, blue, purple, black) neon colors
+	position OrderedPair
+	stage    int     // 0 = egg, 1 = instar1, 2 = instar2, 3 = instar3, 4 = instar4, 5 = adult, 6= dead
+	energy   float64 // Degree-days
+	isAlive  bool
+	color    Color // color to show on scatter plot (red, orange, yellow, green, blue, purple, black) neon colors
 
 }
 type Color struct {
@@ -30,21 +29,6 @@ type stage struct {
 type OrderedPair struct {
 	x float64
 	y float64
-}
-
-// QuadTree simply contains a pointer to the root.
-// Another way of doing this would be type QuadTree *Node
-type QuadTree struct {
-	root *Node
-}
-
-// Node object contains a slice of children (this could just as easily be an array of length 4).
-// A node refers to a star. Sometimes, the star will be a "dummy" star, sometimes it is a star in the
-// universe, and sometimes it is nil. Every internal node points to a dummy star.
-type Node struct {
-	children []*Node
-	fly      *Fly
-	sector   Quadrant
 }
 
 // Quadrant is an object representing a sub-square within a larger universe.
